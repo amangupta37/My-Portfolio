71d7091ae98377d38009d779dc9b7b12f41db9ca
amangupta37 <amangupta6121998@gmail.com>
2020-11-27 11:34:43 +0530

    added js file

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+//
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+				'verticalCentered': false,
+				'scrollingSpeed': 1000000000000000,
+				'autoScrolling': false,
+				'css3': true,
+				'navigation': true,
+				'navigationPosition': 'right',
+			});
+		});
+
+// wow
+$(function()
+{
+    new WOW().init();
+    $(".rotate").textrotator();
+})
