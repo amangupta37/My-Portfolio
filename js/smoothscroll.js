7815f8cd2a62a80843e7589d21eed95f93caaeac
amangupta37 <amangupta6121998@gmail.com>
2020-11-29 19:48:40 +0530

    updated js folder

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -235,18 +235,4 @@ jQuery(function($) {
   $.localScroll({ filter: ".smoothScroll" });
 });
 
-var btn = $('#back-to-top');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
 
